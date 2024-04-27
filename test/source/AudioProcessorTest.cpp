2ec366ae1ba4a77c28b2a5a5b6a47177b3bbca28
rokussadee <rokus.sadee@student.ehb.be>
2024-04-27 21:39:23 +0200

    initial commit: C++, CMake, CPM, JUCE, clang-format pre-commit hook

---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,11 @@
+#include <JuceHeader.h>
+
+#include <VirtualBand/PluginProcessor.h>
+#include <gtest/gtest.h>
+
+namespace virtual_band_test {
+TEST(AudioPluginAudioProcessor, Foo) {
+  AudioPluginAudioProcessor processor{};
+  ASSERT_FALSE(true);
+}
+}  // namespace virtual_band_test
